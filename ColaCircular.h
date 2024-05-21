ce0cef3418e2bd8ced9720907520c2a956068415
juanf <juanf@JUAN>
2024-05-20 20:41:04 -0400

    Add existing file

--- ColaCircular.h
+++ ColaCircular.h
@@ -1,11 +1,10 @@
 #include<iostream>
-#include<conio.h>
-#include<stdio.h>
-#include<string>
 #include<vector>
+#include "tarea.h"
 using namespace std;
 #define max 10
 
+template <typename T>
 class ColaCircular
 {
     public:
@@ -13,15 +12,15 @@ class ColaCircular
     int inicio;
     int fin;
     int cantElementos;
-    int cola[max];
+    std::vector<T> cola;
 
     //contrtuctor
-    ColaCircular();
+    ColaCircular() : inicio(0), fin(0), cantElementos(0) {}
 
     public:
     //metodos
-    int siguiente( int );
-    void push(int);
+    int siguiente( int r );
+    void push(int elemento);
     int pop();
     void borrar();
     void listar();
@@ -32,4 +31,94 @@ class ColaCircular
     
 };
 
+
+
+template <typename T>
+inline int siguiente(int r)
+{  
+    return (r+1) % max;
+}
+
+template <typename T>
+inline void push(int elemento)
+{
+    if(cola_llena()){
+        cout << "ERROR, Cola llena \n";
+    } else {
+        fin = siguiente[fin];
+        cola[fin] = elemento;
+        cantElementos++;
+    }
+}
+
+template <typename T>
+inline int pop()
+{
+    if(!cola_vacia()){
+        int tm = cola[inicio];
+        inicio = siguiente(inicio);
+        cantElementos--;
+        return tm;
+    } else {
+        cout << "ERROR, Cola vacia \n";
+        return 0;
+    }
+}
+
+template <typename T>
+inline void borrar()
+{
+    inicio = 0;
+    fin = max -1;
+    cantElementos = 0;
+}
+
+template <typename T>
+inline void listar()
+{
+    cout << "Inicio: " << inicio << "  Fin: " << fin << endl << endl;
+    if(!cola_vacia()){
+       if(inicio <= fin){
+         for (int i = inicio; i <= inicio; i++)
+        {
+            cout << " \t\t" << cola[i] << endl;
+        }        
+        } else {
+            for (int i = 0; i <= fin; i++)
+            {
+            cout << " \t\t" << cola[i] << endl;
+            }
+
+            for (int i = inicio; i < max; i++)
+            {
+            cout << " \t\t" << cola[i] << endl;
+            }
+        }   
+    } else {
+        cout << "Cola vacia \n";
+    }
+}
+
+template <typename T>
+inline int frente()
+{
+    if(!cola_vacia()){
+        return cola[inicio];
+    } else {
+        cout << "ERROR, Cola vacia \n";
+        return 0;
+    }
+}
+
+template <typename T>
+inline int cola_llena()
+{
+    return cantElementos == max; 
+}
+
+template <typename T>
+inline int cola_vacia()
+{
+    return cantElementos == 0;
+}
 
